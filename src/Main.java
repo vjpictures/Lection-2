49fa2a9207189f868cb4c7f00d45bd463d281d33
vjpictures <vjpictures@mail.ru>
2024-01-01 20:22:46 +0100

    added the solution of task 1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,29 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        double sum=0;
+        int numberSubjects;
+        double averageMark;
+        Scanner NumScanner = new Scanner(System.in);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Введите количество предметов: ");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        numberSubjects = NumScanner.nextInt();
+
+        for (int x = 1; x <= numberSubjects; x++) {
+            System.out.println("Введите оценку за " + x + " предмет:");
+            int mark = NumScanner.nextInt();
+            if (mark > 10 || mark <= 0) {
+
+                System.out.println("Ошибка! Введите оценку от 1 до 10.");
+            }
+
+            sum = sum + mark;
+            System.out.println();
         }
+
+        averageMark = (sum / numberSubjects);
+        System.out.println("Средняя оценка: " + averageMark);
     }
 }
