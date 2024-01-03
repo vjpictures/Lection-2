ffba14bbaad73bc341d4e955e2967a786d505ede
vjpictures <vjpictures@mail.ru>
2024-01-03 20:20:15 +0100

    changes in the solution of task 1

--- src/lection2/Main.java
+++ src/lection2/Main.java
@@ -1,26 +1,27 @@
+package lection2;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double sum=0;
+        double sum = 0;
         int numberSubjects;
         double averageMark;
-        Scanner NumScanner = new Scanner(System.in);
+        Scanner Numscanner = new Scanner(System.in);
 
         System.out.println("Введите количество предметов: ");
 
-        numberSubjects = NumScanner.nextInt();
+        numberSubjects = Numscanner.nextInt();
 
         for (int x = 1; x <= numberSubjects; x++) {
-            System.out.println("Введите оценку за " + x + " предмет:");
-            int mark = NumScanner.nextInt();
+            System.out.println("\nВведите оценку за " + x + " предмет:");
+            int mark = Numscanner.nextInt();
             if (mark > 10 || mark <= 0) {
 
                 System.out.println("Ошибка! Введите оценку от 1 до 10.");
             }
 
             sum = sum + mark;
-            System.out.println();
         }
 
         averageMark = (sum / numberSubjects);
